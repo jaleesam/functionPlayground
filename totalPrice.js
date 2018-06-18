a80eca39082176bc8c987ff84aeee58d2bf91722
Jaleesa McCrea <jmccrea01@gmail.com>
2018-06-18 02:44:24 -0400

    more code

--- totalPrice.js
+++ totalPrice.js
@@ -1,19 +1,23 @@
-// calucing the price of phone purchases
+// calculating the price of phone purchases
 // keep buying the same phone purchase until you run out money
-// also buy 
+// also buy a phone case to go with this phone
 
 const bestPhone = 250.00;
 const tax = 0.08;
 let bankBalance = 1100.00;
 const phoneCase = 20.00;
 let total = 0;
+let  totalPhoneCost = 0;
+let newTotal = 0;
 
 function totalCost() {
     totalTax= bestPhone * tax;
     total = bestPhone + totalTax;
     total= "$" + total.toFixed(2);
-    return total;
+    console.log(total);
+    return total;    
 }
+
 
 
 function phoneAndTax() {
@@ -23,7 +27,26 @@ function phoneAndTax() {
 }
 
 function buyAcces() {
-    
-}
+    totalPhoneCost = phoneAndTax();
+    totalAcces = phoneCase * tax;
+    newTotal = totalPhoneCost + totalAcces;
+    if (bankBalance > newTotal) {
+        console.log("Yes, I want the case.")
+        bankBalance = bankBalance - newTotal;
+        return bankBalance;
+    }
+    else {
+        console.log("No I thank you. I don't want a case.");
+    }
+};
 
 
+
+function buyMultiPhones() {
+    while (bankBalance > newTotal) {
+        buyAcces();
+        console.log("New bank balance: $"+ bankBalance.toFixed(2));
+    }
+};
+
+buyMultiPhones();
