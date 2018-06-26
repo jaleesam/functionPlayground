a5352436a5488b1d80a08f4efb74bfd7ef5deacc
Jaleesa McCrea <jmccrea01@gmail.com>
2018-06-25 23:37:15 -0400

    code

--- reactTicTacToe/my-app/src/index.js
+++ reactTicTacToe/my-app/src/index.js
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            value: null,
-        };
-    }
     render() {
         return(
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+        <button className="square" 
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
         </button>
         );
     }
@@ -25,6 +20,13 @@ class Board extends React.Component{
             squares: Array(9).fill(null),
         };
     }
+
+    handleClick(i) {
+        const squares =this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares:squares});
+    }
+
     renderSquare(i) {
         return (
         <Square value={this.state.squares[i]} 
@@ -39,19 +41,13 @@ class Board extends React.Component{
             <div>
                 <div className="status">{status}</div>
                 <div className="board-now">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
+                {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
                 </div>
                 <div className="board-now">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
+                {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
                 </div>
                 <div className="board-now">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
+                {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
                 </div>
             </div>
         );
